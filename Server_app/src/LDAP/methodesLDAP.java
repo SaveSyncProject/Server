e8cb5fe747ec9a513afb416a8a6b84e9d3fcdd46
gaetanana <108981554+gaetanana@users.noreply.github.com>
2023-12-13 20:19:28 +0100

    ldif mis à jour + début LDAP

--- Server_app/src/LDAP/methodesLDAP.java
+++ Server_app/src/LDAP/methodesLDAP.java
@@ -4,40 +4,26 @@ import javax.naming.NamingException;
 import javax.naming.directory.DirContext;
 import javax.naming.directory.InitialDirContext;
 import java.util.Hashtable;
+import com.unboundid.ldap.sdk.LDAPConnection;
+import com.unboundid.ldap.sdk.LDAPException;
 
 public class methodesLDAP {
-    public static boolean authenticate(String username, String password) {
-        String ldapUrl = "ldap://ldap-server";
-        String domain = "example.org";
 
-        // Configuration des propriétés de l'environnement pour la connexion LDAP
-        Hashtable<String, String> env = new Hashtable<>();
-        env.put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.ldap.LdapCtxFactory");
-        env.put(Context.PROVIDER_URL, ldapUrl);
-        env.put(Context.SECURITY_AUTHENTICATION, "simple");
-        env.put(Context.SECURITY_PRINCIPAL, username + "@" + domain); // Utiliser le format 'username@domain'
-        env.put(Context.SECURITY_CREDENTIALS, password);
-
+    private boolean authenticateWithLDAP(String username, String password) {
         try {
-            // Tentative de connexion au serveur LDAP
-            DirContext ctx = new InitialDirContext(env);
-            System.out.println("Connexion réussie!");
-            ctx.close();
-            return true; // Authentification réussie
-        } catch (NamingException e) {
-            System.out.println("Erreur lors de la connexion: " + e.getMessage());
-            return false; // Authentification échouée
+            LDAPConnection connection = new LDAPConnection("localhost", 389);
+            connection.bind("uid=" + username + ",ou=utilisateurs,dc=example,dc=org", password);
+            connection.close();
+            return true;
+        } catch (LDAPException e) {
+            e.printStackTrace();
+            return false;
         }
     }
 
     public static void main(String[] args) {
-        // Exemple d'utilisation
-        boolean isAuthenticated = authenticate("ggonfiantini", "gaga"); // Remplacer par les crédentials réels
-        if (isAuthenticated) {
-            System.out.println("Utilisateur authentifié avec succès.");
-        } else {
-            System.out.println("Échec de l'authentification.");
-        }
+          methodesLDAP m = new methodesLDAP();
+          System.out.println(m.authenticateWithLDAP("ggonfiantini", "gaga"));
     }
 
 }
