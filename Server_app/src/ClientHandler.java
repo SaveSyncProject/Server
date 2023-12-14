12e7cff3e5ee8e26fe472deca2633b0df94eadcc
gaetanana <gaetan.gonfiantini@gmail.com>
2023-12-14 01:09:50 +0100

    Push

--- Server_app/src/ClientHandler.java
+++ Server_app/src/ClientHandler.java
@@ -4,6 +4,10 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 
+import com.unboundid.ldap.sdk.LDAPConnection;
+import com.unboundid.ldap.sdk.LDAPException;
+import model.User;
+
 class ClientHandler extends Thread {
     private Socket clientSocket;
 
@@ -11,14 +15,47 @@ class ClientHandler extends Thread {
         this.clientSocket = socket;
     }
 
+    private static boolean authenticateWithLDAP(String username, String password) {
+        try {
+            LDAPConnection connection = new LDAPConnection("localhost", 389);
+            connection.bind("uid=" + username + ",ou=utilisateurs,dc=example,dc=org", password);
+            connection.close();
+            return true;
+        } catch (LDAPException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public static void main(String[] args){
+        System.out.println(authenticateWithLDAP("ggonfiantini", "gaga"));
+    }
+
+
     public void run() {
         try (BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
              PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true)) {
 
-            String inputLine;
-            while ((inputLine = in.readLine()) != null) { // Lire les messages du client
-                System.out.println("Client dit: " + inputLine);
-                out.println("Echo: " + inputLine); // Répondre au client
+            // Demander le nom d'utilisateur et le mot de passe
+            out.println("Entrez votre nom d'utilisateur:");
+            String username = in.readLine();
+            out.println("Entrez votre mot de passe:");
+            String password = in.readLine();
+
+            User user = new User(username, password);
+
+            // Authentifier l'utilisateur
+            if (authenticateWithLDAP(user.getUsername(), user.getPassword())) {
+                out.println("Authentification réussie");
+
+                // Gérer la communication client après l'authentification
+                String inputLine;
+                while ((inputLine = in.readLine()) != null) {
+                    System.out.println("Client dit: " + inputLine);
+                    out.println("Echo: " + inputLine);
+                }
+            } else {
+                out.println("Authentification échouée");
             }
         } catch (IOException e) {
             System.out.println("Erreur lors de la communication avec le client: " + e.getMessage());
--- Server_app/src/model/User.java
+++ Server_app/src/model/User.java
@@ -8,4 +8,12 @@ public class User {
         this.username = username;
         this.password = password;
     }
+
+    public String getUsername(){
+        return this.username;
+    }
+
+    public String getPassword(){
+        return this.password;
+    }
 }
